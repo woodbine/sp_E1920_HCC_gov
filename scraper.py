feec17b3fda70124813f77ec7f129422b7880c26
woodbine <ian@spendnetwork.com>
2015-04-24 20:30:03 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -44,6 +44,9 @@ for pageLink in pageLinks:
 			encoded_link = parsed_link.geturl()
 			# create the right strings for the new filename
 			csvYr = title.split(' ')[-4]
+			if csvYr=='13':
+				csvYr = '2013'
+			
 			csvMth = title.split(' ')[-5][:3]
 			csvMth = csvMth.upper()
 			csvMth = convert_mth_strings(csvMth);
